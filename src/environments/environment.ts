c4f1b6597bc484ecada25d5ff03825f41d94d516
BryanPool99 <bryanpool991@hotmail.com>
2026-09-24 20:45:09 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: '/api/v1',
+  neonAuthUrl:
+    'https://ep-withered-salad-acdqzx2b.neonauth.sa-east-1.aws.neon.tech/foliobuilderdevbd/auth',
+};
